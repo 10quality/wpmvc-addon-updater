149d7059e6db290a215df3493012182a9f600fda
Alejandro Mostajo <amostajo@gmail.com>
2019-02-07 16:58:55 -0600

    Localization.
    Fixes localization for button.

--- assets/views/button.php
+++ assets/views/button.php
@@ -5,10 +5,10 @@
  * @author Cami Mostajo
  * @package WPMVC\Addons\Updater
  * @license MIT
- * @version 1.0.1
+ * @version 1.0.3
  */
 ?>
 <a role="button"
     href="<?= $url ?>"
     class="button add-wpmvc-updater <?= $class ?> updater-type-<?= $type ?> updater-folder-<?= $folder ?> updater-namespace-<?= $namespace ?>"
-><span class="dashicons"></span><?php _e( 'Update', 'addon' ) ?></a>
+><span class="dashicons"></span><?php _e( 'Update' ) ?></a>
